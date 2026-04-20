65f103b4ddc88ad34f87d41c037511a3ddfb06cd
jsilverhand <worldsoeffective@gmail.com>
2026-04-20 13:39:18 +0300

    feat: commented routes

--- config/routes.php
+++ config/routes.php
@@ -3,7 +3,7 @@
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 return static function (RoutingConfigurator $routes): void {
-    $routes->import('../src/Infrastructure/Http/*/Action/*', 'attribute');
-    $routes->import('../src/Infrastructure/Http/*/Action/*/*', 'attribute');
-    $routes->import('../src/Infrastructure/Http/*/Action/*/*/*', 'attribute');
+//    $routes->import('../src/Infrastructure/Http/*/Action/*', 'attribute');
+//    $routes->import('../src/Infrastructure/Http/*/Action/*/*', 'attribute');
+//    $routes->import('../src/Infrastructure/Http/*/Action/*/*/*', 'attribute');
 };

62638aaa3cd7a7453e4fce2b645f620c528b2a68
jsilverhand <worldsoeffective@gmail.com>
2026-04-20 17:32:59 +0300

    feat: commented infrastructure

--- config/services.php
+++ config/services.php
@@ -10,9 +10,9 @@ return static function (ContainerConfigurator $container): void {
 
     $container->import('packages/*.php');
     $container->import('domain/*.php');
-    $container->import('infrastructure/*.php');
-    $container->import('infrastructure/*/*.php');
-    $container->import('infrastructure/*/*/*.php');
+    //    $container->import('infrastructure/*.php');
+    //    $container->import('infrastructure/*/*.php');
+    //    $container->import('infrastructure/*/*/*.php');
 
     if ('test' === $container->env()) {
         $container->import('packages/test/*.php');
